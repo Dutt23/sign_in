e68b88ff24cd892305bedc0f68bf1ebbb89cc87d
dutt23 <shatyaki_dutt@hotmail.com>
2020-08-01 17:06:40 +0530

    intialize express and dependencies

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require("express");
-
-
 const app = express();
+const path = require("path");
+var cookieParser = require('cookie-parser');
+const methodOverride = require('method-override')
+require('module-alias/register')
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(cookieParser())
+app.use(methodOverride())
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
 
  module.exports = app;
